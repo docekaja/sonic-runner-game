4745b91eb2dfe497ff1620c36662e87c7483313d
Jan Docekal <docekal26@gmail.com>
2024-10-17 20:53:52 +0200

    feat: initialize kaplay loadSprite, loadFont and loadSound

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,46 @@
 import k from "./kaplayCtx";
 
-k
+k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
+k.loadSprite("platforms", "graphics/platforms.png");
+k.loadSprite("sonic", "graphics/sonic.png", {
+    sliceX: 8,
+    sliceY: 2,
+    anims: {
+        run: {
+            from: 0, to: 7, loop: true, speed: 30
+        },
+        jump: {
+            from: 8, to: 15, loop: true, speed: 100
+        }
+    }
+}
+);
+k.loadSprite("ring", "graphics/ring.png", {
+    sliceX: 16,
+    sliceY: 1,
+    anims: {
+        spin: {
+            from: 0, to: 15, loop: true, speed: 30
+        }
+    }
+}
+);
+k.loadSprite("motobug", "graphics/motobug.png", {
+    sliceX: 5,
+    sliceY: 1,
+    anims: {
+        run: {
+            from: 0, to: 4, loop: true, speed: 8
+        },
+    }
+}
+);
+k.loadFont("mania", "fonts/mania.ttf");
+k.loadSound("city", "sound/city.mp3");
+k.loadSound("destroy", "sound/Destroy.mp3");
+k.loadSound("hurt", "sound/Hurt.mp3");
+k.loadSound("hyper-ring", "sound/HyperRing.mp3");
+k.loadSound("jump", "sound/Jump.mp3");
+k.loadSound("ring", "sound/Ring.mp3");
+
+
